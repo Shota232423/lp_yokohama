250f44eb59f2695e00ee72310ef03860897454d6
kame <midorima570@gmail.com>
2021-08-23 13:56:23 +0900

    spヘッダー修正

--- js/index.js
+++ js/index.js
@@ -7,7 +7,9 @@ $(function(){
     $('.hamburger_area').click(function(){
         $('nav').toggleClass('open');
         $('.hamburger_area').toggleClass('stop_scroll');
+
         if($('.hamburger_area').hasClass('stop_scroll')){
+        $('#hamburger_bg').toggleClass('no_display');//no_display削除
         // スクロール禁止(SP)
         document.addEventListener('touchmove', noScroll, {
             passive: false
@@ -17,6 +19,7 @@ $(function(){
             passive: false
         });
         }else{
+            $('#hamburger_bg').toggleClass('no_display');//no_display追加
             document.removeEventListener('touchmove', noScroll, {
                 passive: false
             });
@@ -25,5 +28,20 @@ $(function(){
                 passive: false
             });
     }
+    });
+
+    $('#hamburger_bg').click(function(){
+        $('nav').toggleClass('open');
+        $('.hamburger_area').toggleClass('stop_scroll');
+        $('#hamburger_bg').toggleClass('no_display');
+
+        document.removeEventListener('touchmove', noScroll, {
+            passive: false
+        });
+        // スクロール禁止を解除(PC)
+        document.removeEventListener('mousewheel', noScroll, {
+            passive: false
+        });
+
     });
 });
